d30f4f16e72c45051b7b2b13ceb97f7b581bbdbd
marcelooliveira <mclricardo@gmail.com>
2017-02-12 21:22:03 -0200

    primeiro commit
    primeiro commit

---
+++ src/Carrinho/webpack.config.js
@@ -0,0 +1,29 @@
+﻿var path = require('path');
+
+module.exports = {
+    entry: 
+    {
+        javascript: [
+            './wwwroot/js/Cart.jsx',
+            './wwwroot/js/CheckoutSuccess.jsx',
+            './wwwroot/js/Components.jsx'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'wwwroot/build'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
